e53da459f84a6302e8b0d182f04bba34cc41aac8
fil <filipp.kosenko@1win.pro>
2023-12-22 11:22:26 +0300

    fil fix

--- src/components/RequestForm/index.tsx
+++ src/components/RequestForm/index.tsx
@@ -1,4 +1,6 @@
 import "./RequestForm.sass";
+import "../SASS/button.sass"
+import "../SASS/title.sass"
 import { useState } from "react";
 import axios from "axios";
 
@@ -22,36 +24,36 @@ export const RequestForm = () => {
 
   return (
     <div className="request-container">
-      <h2 className="request-title">
-        Получите бесплатную консультацию по подбору пульсометра
-      </h2>
-      <p>
-        Просто заполните форму заявки и мы перезвоним вам в течении 10 минут
-      </p>
-      <input
-        type="text"
-        placeholder="Введите имя"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        className="request-input"
-      />
-      <input
-        type="text"
-        placeholder="Введите номер телефона"
-        value={phoneNumber}
-        onChange={(e) => setPhoneNumber(e.target.value)}
-        className="request-input"
-      />
-      <input
-        type="text"
-        placeholder="Введите Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        className="request-input"
-      />
-      <button className="request-button" onClick={handleSubmit}>
+      <div className="cont">
+      <h2 className="title title__left">Получите бесплатную консультацию<br/>по подбору пульсометра </h2>
+      <div className="consultation__descr">Просто заполните форму заявки <br/>и мы перезвоним вам в течении 10 минут</div>
+      <form id="section-form"action="#" className="feed-form">
+        <input
+          type="text"
+          placeholder="Введите имя"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          className="request-input"
+        />
+        <input
+          type="text"
+          placeholder="Введите номер телефона"
+          value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)}
+          className="request-input"
+        />
+        <input
+          type="text"
+          placeholder="Введите Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="request-input"
+        />
+      </form>
+      <button className="button button_submit" onClick={handleSubmit}>
         Заказать консультацию
       </button>
+      </div>
     </div>
   );
 };
--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,6 +1,10 @@
 import { Header } from "../../components/Header";
+import { Advantages } from "../../components/Advantages";
 import { RequestForm } from "../../components/RequestForm";
 import { ShopItem } from "../../components/ShopItem";
+import { Reviews } from "../../components/Reviews";
+import { Footer } from "../../components/Footer";
+import  Modal  from "../../components/Modal";
 import "./MainPage.sass";
 import { useState, useEffect } from "react";
 import axios from "axios";
@@ -14,9 +18,17 @@ type ShopItemData = {
   filter: string;
 };
 
+
+
+
 export const MainPage = () => {
   const [shopItems, setShopItems] = useState<ShopItemData[]>([]);
   const [selectedFilter, setSelectedFilter] = useState<string | null>(null);
+
+  const [isOpen, setIsOpen] = useState(false);
+  const closeModal = () => {
+    setIsOpen(false);
+  };
 
   useEffect(() => {
     axios
@@ -39,43 +51,53 @@ export const MainPage = () => {
 
   return (
     <div className="main-container">
-      <Header />
+      <Header/>    
+      <Advantages />
       <RequestForm />
       <div className="main-items">
         <h1>Каталог пульсометров</h1>
         <div className="main-filters">
-          <button
-            className="main-filter-button"
-            onClick={() => handleFilterClick("fitness")}
-          >
-            Для фитнеса
-          </button>
+         
+            <button
+              className="main-filter-button"
+              onClick={() => handleFilterClick("fitness")}
+            >
+              <div>Для фитнеса</div>
+            </button>
+          
           <button
             className="main-filter-button"
             onClick={() => handleFilterClick("running")}
           >
-            Для бега
+            <div>Для бега</div>
           </button>
           <button
             className="main-filter-button"
             onClick={() => handleFilterClick("triathlon")}
           >
-            Для триатлона
+           <div>Для триатлона</div> 
           </button>
         </div>
-        <div className="main-item-selection">
-          {filteredShopItems.map((shopItem: ShopItemData) => {
-            const props = {
-              id: shopItem.id,
-              title: shopItem.title,
-              itemImg: shopItem.itemImg,
-              description: shopItem.description,
-              price: shopItem.price,
-            };
-            return <ShopItem {...props} />;
-          })}
+        <div className="cont cont-cat">
+          <div className="main-item-selection">
+            {filteredShopItems.map((shopItem: ShopItemData) => {
+              const props = {
+                id: shopItem.id,
+                title: shopItem.title,
+                itemImg: shopItem.itemImg,
+                description: shopItem.description,
+                price: shopItem.price,
+              };
+              return <ShopItem {...props} />;
+            })}
+          </div>
         </div>
       </div>
+      < Reviews/>
+      < Footer/>
+      <Modal isOpen={isOpen} onClose={closeModal} />
     </div>
   );
+
+  
 };
